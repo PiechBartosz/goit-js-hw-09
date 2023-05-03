44ed3db2178bc90b01ad959176a21976b69167f1
PiechBartosz <118010428+PiechBartosz@users.noreply.github.com>
2023-05-03 16:27:19 +0000

    Deploying to gh-pages from @ PiechBartosz/goit-js-hw-09@9b56607e0da3cf586e45a945c5f1aaf382521dd4 🚀

---
+++ 01-color-switcher.b8425e53.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");function o(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.addEventListener("click",(function(){t=setInterval(o,1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.b8425e53.js.map
