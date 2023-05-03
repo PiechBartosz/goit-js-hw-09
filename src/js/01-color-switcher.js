9b56607e0da3cf586e45a945c5f1aaf382521dd4
PiechBartosz <p.bartosz@yahoo.com>
2023-05-03 18:26:58 +0200

    upgrade

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let intervalId;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+startButton.addEventListener('click', () => {
+  intervalId = setInterval(changeBackgroundColor, 1000);
+  startButton.disabled = true;
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startButton.disabled = false;
+});
